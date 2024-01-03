65c63ee5dc0f921d52769b01ce7f12d0c894e1e5
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2024-01-03 08:06:01 +0530

    COM : FIX :: The mongo connetion string docker network:

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ try {
 }
 
 try {
-  mongoose.connect(process.env.DB_URL);
+  mongoose.connect(process.env.DB_URL_NETWORK);
   console.log(`connected to the database successfully`);
 } catch (error) {
   console.log(`Error while connecting to the database, error : ${error}`);
